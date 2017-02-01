261809a5ab372cfd821af611673387d41c462ad1
Joel Rizzo <milesprower1010@gmail.com>
2017-01-31 21:23:43 -0600

    Improved the addArguments function.
    Changed addArguments to add any arbitrary number of args instead of
    strictly 2.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,14 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide a number of integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        int sum = 0;
+        for (String arg : args)
+            sum += Integer.valueOf(arg);
+        return sum;
     }
 }
